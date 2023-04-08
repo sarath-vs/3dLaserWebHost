bd5f49356bb24be79e55f0948f967c7a6424dd27
sarath sathyabalan <66293762+sarath-vs@users.noreply.github.com>
2023-04-08 12:15:20 +0530

    final assembly

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "24e61ccde29d3959512e8758b3a95972",
-"index.html": "4a5003e47a4f8086e439d69adf2a07ce",
-"/": "4a5003e47a4f8086e439d69adf2a07ce",
-"main.dart.js": "4af2ffcfcd19c70b383a9a51bb011ca0",
+"index.html": "c2cc9fedde93a396531b23ffc1a692ec",
+"/": "c2cc9fedde93a396531b23ffc1a692ec",
+"main.dart.js": "8658d303566045d7e24ca11f1145d213",
 "flutter.js": "a85fcf6324d3c4d3ae3be1ae4931e9c5",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
